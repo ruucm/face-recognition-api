b1eda7683e51837a317e5733a747f3d1e9180edd
ruucm <ruucm.a@gmail.com>
2018-05-01 04:50:27 +0900

    fix: imageAnalysis
    remove log & add region setting

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
 import ImageAnalyser from './lib/imageAnalyser'
 import { save } from './lib/fetchImage'
-import { log } from 'ruucm-util'
+// import { log } from 'ruucm-util'
 
 export const hello = async (event, context, callback) => {
   const response = {
@@ -24,8 +24,8 @@ const message = ({ time, ...rest }) => new Promise((resolve, reject) =>
  * Image Analysis
  */
 export const imageAnalysis = async (event, context, callback) => {
-  log('start Image Save 😀')
-  imageSave('https://cdn.namuwikiusercontent.com/s/67552ce204c3a7be54a6b5376501d43babb21da14b6c58ead22470f769b56543856d39e62e68056dcaba89fa62862d6e02134d361aa5547f2387e7606d8ecee1c85046b46d8165fed63018a0ad14eb16?e=1533377835&k=9FVwiwYz-MRz9GdMWdOEvg')
+  // log('hey!!!')
+  save('https://cdn.namuwikiusercontent.com/s/67552ce204c3a7be54a6b5376501d43babb21da14b6c58ead22470f769b56543856d39e62e68056dcaba89fa62862d6e02134d361aa5547f2387e7606d8ecee1c85046b46d8165fed63018a0ad14eb16?e=1533377835&k=9FVwiwYz-MRz9GdMWdOEvg')
   const data = JSON.parse(event.body);
 
   const s3Config = {
