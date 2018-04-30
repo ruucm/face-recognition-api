68f0e8bf041fb6fece796f7b721d6f50e4f61444
ruucm <ruucm.a@gmail.com>
2018-05-01 08:29:09 +0900

    chore: imageAnalysis
    update ruucm-util

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
 import ImageAnalyser from './lib/imageAnalyser'
 import { save } from './lib/fetchImage'
-// import { log } from 'ruucm-util'
+import { log } from 'ruucm-util'
 
 export const hello = async (event, context, callback) => {
   const response = {
@@ -24,13 +24,13 @@ const message = ({ time, ...rest }) => new Promise((resolve, reject) =>
  * Image Analysis
  */
 export const imageAnalysis = async (event, context, callback) => {
-  // log('hey!!!')
-  save('https://cdn.namuwikiusercontent.com/s/67552ce204c3a7be54a6b5376501d43babb21da14b6c58ead22470f769b56543856d39e62e68056dcaba89fa62862d6e02134d361aa5547f2387e7606d8ecee1c85046b46d8165fed63018a0ad14eb16?e=1533377835&k=9FVwiwYz-MRz9GdMWdOEvg')
+  log('start save Image 😀')
+  save('https://avatars0.githubusercontent.com/u/29548023?s=460&v=4')
   const data = JSON.parse(event.body);
 
   const s3Config = {
     bucket: 'anyfiles-for-ruucm',
-    imageName: 'test-item-01.jpg',
+    imageName: 'test-item-03.jpg',
   };
   return ImageAnalyser
     .getImageLabels(s3Config)
