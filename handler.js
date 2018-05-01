a504513d0c7104c9603a321b66107947949c8aa8
ruucm <ruucm.a@gmail.com>
2018-05-01 18:22:00 +0900

    fmt: imageAnalysis
    remove unusing code

--- handler.js
+++ handler.js
@@ -27,42 +27,37 @@ export const imageAnalysis = async (event, context, callback) => {
 
   let imageName = '';
 
-
-  // log('event', event)
-
   const query = event.queryStringParameters || {};
   log('query', query)
-
-  // log('event', event)
-
   log('start save Image 😀')
-  if (query.url)
-    imageName = save(query.url)
+  if (query.url) {
+    imageName = await save(query.url)
+    log('End save Image 🖼')
+    log('imageName2', imageName)
+    log(imageName)
+
+    const s3Config = {
+      bucket: 'anyfiles-for-ruucm',
+      imageName: imageName,
+    };
+    log('return ImageAnalyser')
+    return ImageAnalyser
+      .getImageLabels(s3Config)
+      .then((labels) => {
+        const response = {
+          statusCode: 200,
+          body: JSON.stringify({ Labels: labels }),
+        };
+        callback(null, response);
+      })
+      .catch((error) => {
+        callback(null, {
+          statusCode: error.statusCode || 501,
+          headers: { 'Content-Type': 'text/plain' },
+          body: error.message || 'Internal server error',
+        });
+      });
+  }
   else
     log('no url')
-
-  log('imageName2', imageName)
-
-  const data = JSON.parse(event.body);
-
-  const s3Config = {
-    bucket: 'anyfiles-for-ruucm',
-    imageName: imageName,
-  };
-  return ImageAnalyser
-    .getImageLabels(s3Config)
-    .then((labels) => {
-      const response = {
-        statusCode: 200,
-        body: JSON.stringify({ Labels: labels }),
-      };
-      callback(null, response);
-    })
-    .catch((error) => {
-      callback(null, {
-        statusCode: error.statusCode || 501,
-        headers: { 'Content-Type': 'text/plain' },
-        body: error.message || 'Internal server error',
-      });
-    });
 };
