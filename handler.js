fec5624626b2b5073eacc39c9985a61776065a76
ruucm <ruucm.a@gmail.com>
2018-05-01 22:30:02 +0900

    feat: imageAnalysis
    complete label detect

--- handler.js
+++ handler.js
@@ -23,6 +23,39 @@ const message = ({ time, ...rest }) => new Promise((resolve, reject) =>
 /**
  * Image Analysis
  */
+
+
+var request = require('request');
+var hget = require('hget');
+var marked = require('marked');
+var Term = require('marked-terminal');
+
+function getRandomPonyFooArticle () {
+  return new Promise((resolve, reject) => {
+    request('https://ponyfoo.com/articles/random', (err, res, body) => {
+      if (err) {
+        reject(err); return;
+      }
+      resolve(body);
+    });
+  });
+}
+
+
+const read = async () => {
+  var html = await getRandomPonyFooArticle();
+  var md = hget(html, {
+    markdown: true,
+    root: 'main',
+    ignore: '.at-subscribe,.mm-comments,.de-sidebar'
+  });
+  var txt = marked(md, {
+    renderer: new Term()
+  });
+  console.log(txt);
+}
+
+
 export const imageAnalysis = async (event, context, callback) => {
 
   let imageName = '';
